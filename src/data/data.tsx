cc2d4978bb7918ec98bc776adf209e703909aacd
Satyasrirama Siva Krishna Sanam <sanam1@usf.edu>
2024-09-08 13:36:46 -0400

    adding the files

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -19,8 +19,8 @@ import porfolioImage2 from '../images/portfolio/portfolio-2.jpg';
 import porfolioImage3 from '../images/portfolio/portfolio-3.jpg';
 import porfolioImage4 from '../images/portfolio/portfolio-4.jpg';
 import porfolioImage5 from '../images/portfolio/portfolio-5.jpg';
-/*import porfolioImage6 from '../images/portfolio/portfolio-6.jpg';
-import porfolioImage7 from '../images/portfolio/portfolio-7.jpg';
+import porfolioImage6 from '../images/portfolio/portfolio-6.jpg';
+/*import porfolioImage7 from '../images/portfolio/portfolio-7.jpg';
 import porfolioImage8 from '../images/portfolio/portfolio-8.jpg';
 import porfolioImage9 from '../images/portfolio/portfolio-9.jpg';
 import porfolioImage10 from '../images/portfolio/portfolio-10.jpg';
@@ -223,6 +223,12 @@ export const portfolioItems: PortfolioItem[] = [
         description: 'An interactive website where customers can rent their favourite car',
         url: 'http://myweb.usf.edu/~cv628/ASSIGNMENT-3/index.html',
         image: porfolioImage5,
+    },
+    {
+        title: 'Music Store SQL Analysis',
+        description: 'MySQL analysis to optimize data from an online music store.',
+        url: 'https://github.com/Siva-Sanam/Data_Analysis_SQL_Project_Music_Store_Sales',
+        image: porfolioImage6,
     },
 ];
 
